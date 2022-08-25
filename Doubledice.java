46059e0ab487d701eea2990436860c14dbafcba4
ztype764 <49955693+ztype764@users.noreply.github.com>
2022-08-26 00:36:09 +0530

    Add files via upload
    doublediceroll in java

---
+++ Doubledice.java
@@ -0,0 +1,9 @@
+package ba;
+
+public class Doubledice {
+ public static void main(String[] args) {
+	int a;
+	a=Roll.call();
+	System.out.println(a);
+}
+}
---
+++ Roll.java
@@ -0,0 +1,15 @@
+package ba;
+import java.util.Random;
+public class Roll {
+	static int call() {
+		int d1,d2=0;
+	System.out.println("Rolling dice!");
+	Random ran=new Random();
+	d1=1+ran.nextInt(6);
+	if(d1==6) {
+		System.out.println("6!Roll another!");
+		d2=1+ran.nextInt(6);
+	}
+	return d1+d2;
+}
+}
